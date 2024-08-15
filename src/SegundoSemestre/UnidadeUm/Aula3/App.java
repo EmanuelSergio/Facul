3fdf8768f581046fc93b99619daaf511af8c3cc0
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-08-14 21:07:54 -0300

    app

---
+++ src/SegundoSemestre/UnidadeUm/Aula3/App.java
@@ -0,0 +1,34 @@
+package SegundoSemestre.UnidadeUm.Aula3;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner(System.in);
+
+        Pessoa pessoas[] = new Pessoa[3];
+        
+
+        for (int i = 0; i < pessoas.length; i++) {
+            pessoas[i] = new Pessoa();
+
+            System.out.println("Informe o Nome da "+(i+1) +"ª pessoa");
+            pessoas[i].setNome(leia.next());
+            
+            System.out.println("Informe o altura da "+(i+1) +"ª pessoa");
+            pessoas[i].setAltura(leia.nextDouble());
+            
+            System.out.println("Informe o peso da "+(i+1) +"ª pessoa");
+            pessoas[i].setPeso(leia.nextDouble());
+        }
+
+        for (int i = 2; i >= 0; i--) {
+            System.out.println(pessoas[i].getNome()+":\n");
+            System.out.printf("IMC: %.2f\n", pessoas[i].calcularImc());
+            System.out.println();
+        }
+
+        leia.close();
+    }   
+}
