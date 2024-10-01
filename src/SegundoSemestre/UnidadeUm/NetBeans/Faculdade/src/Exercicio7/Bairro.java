023c91f132380f52bc16f4320a1bd02f61eb06b1
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-09-30 22:42:32 -0300

    inicio

---
+++ src/SegundoSemestre/UnidadeUm/NetBeans/Faculdade/src/Exercicio7/Bairro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio7;
+
+/**
+ *
+ * @author emanu
+ */
+public class Bairro {
+    
+    private String nome;
+    private Double coeficienteIptu;
+    
+    
+    
+    
+}
