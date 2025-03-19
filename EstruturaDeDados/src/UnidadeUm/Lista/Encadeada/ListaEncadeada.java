11dce53c5a22e42ed0c22fc40dd7c582dfbf2813
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2025-03-19 10:05:51 -0300

    Refactor ListaEncadeada class: improve method formatting, implement buscar and retirar methods

--- EstruturaDeDados/src/UnidadeUm/Lista/Encadeada/ListaEncadeada.java
+++ EstruturaDeDados/src/UnidadeUm/Lista/Encadeada/ListaEncadeada.java
@@ -8,7 +8,7 @@ public class ListaEncadeada <T>{
         this.primeiro = null;
     }
 
-    public NoLista<T> getPrimeiro() {
+    public NoLista<T> getPrimeiro()  {
         return primeiro;
     }
 
@@ -24,21 +24,45 @@ public class ListaEncadeada <T>{
             return true;
         }
         return false;
+    } 
+
+    public NoLista<T> buscar(T valor){
+         
+        NoLista<T> p = primeiro;
+
+        while (p != null) {
+            if(p.getInfo().equals(valor)){
+                return p;
+            }else{ 
+                p = p.getProximo();
+            }
+        }
+        return null;
     }
 
-    // public NoLista<T> buscar(T valor){
-         
-    //     NoLista p = primeiro;
+    public void retirar(T valor){
+        NoLista<T> anterior = null;
+        NoLista<T> p = primeiro;
 
-    //     while (p != null) {
-    //         if(p.getInfo() == valor){
-    //             return p;
-    //         }
-    //     }
+        // procura nó que contém dado a ser removido,
+        // guardando o anterior
+        while(p != null && !p.getInfo().equals(valor)){
+            anterior = p;
+            p = p.getProximo();
+        }
+
+        // se achou o nó que contém o dado a ser removido
+
+        if(p != null){
+            if(p.equals(primeiro)){
+                this.primeiro = p.getProximo();
+            }else{
+                anterior.setProximo(p.getProximo());
+            }
+        }
         
+    }
 
-
-    //     return valor;
-    // }
+    
 
 }
