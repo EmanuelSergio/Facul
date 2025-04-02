4ae6aa499fe8ab62f951cd31ddad22da4ebcd262
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2025-04-02 15:48:46 -0300

    Update binary class files for various exercises and applications

---
+++ EstruturaDeDados/src/UnidadeUm/Pilha/PilhaCheiaException.java
@@ -0,0 +1,8 @@
+public class PilhaCheiaException extends Exception{
+
+    public PilhaCheiaException(String msg){
+        super(msg);
+    }
+
+
+}
---
+++ EstruturaDeDados/src/UnidadeUm/Pilha/PilhaVetor.java
@@ -0,0 +1,57 @@
+public class PilhaVetor <T> implements Pilha<T> {
+    private Object info[];
+    private int limite;
+    private int tamanho;
+
+    public PilhaVetor(int limite) {
+        this.limite = limite;
+        this.tamanho = 0;
+        this.info = new Object[limite];
+    }
+
+    @Override
+    public void push(T info) {
+        if(limite == tamanho){
+            throw new RuntimeException("Pilha cheia");
+        }
+        this.info[tamanho] = info;
+        tamanho++;
+    }
+
+    @Override  
+    public T pop() {
+        if(estaVazia()){
+            throw new RuntimeException("Pilha vazia");
+        }else{
+            T elemento = (T) info[tamanho - 1];
+            info[tamanho - 1] = null;
+            tamanho--;
+            return elemento;
+        }
+    }
+
+    @Override
+    public T peek() {
+        if(estaVazia()){
+            throw new RuntimeException("Pilha vazia");
+        }else{
+            return (T) info[tamanho - 1];
+        }
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return tamanho == 0;
+    }
+
+    @Override
+    public void liberar() {
+        for(int i = 0; i < tamanho; i++) {
+            info[i] = null;
+        }
+    }
+
+    
+     
+
+}
