b575b8af02ee9f0c1c169f1e3db4b7f36b73e9c7
Emanuel Sergio Girardi <esgirardi@furb.br>
2025-05-07 19:30:31 -0300

    add

---
+++ EstruturaDeDados/src/UnidadeUm/Arvore/exercicio7/ArvoreBinaria.java
@@ -0,0 +1,89 @@
+package UnidadeUm.Arvore.exercicio7;
+
+public class ArvoreBinaria<T> {
+    private NoArvoreBinaria<T> raiz;
+
+    public ArvoreBinaria() {
+        raiz = null;
+    }
+
+    public NoArvoreBinaria<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NoArvoreBinaria<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public boolean estaVazia() {
+        return getRaiz() == null;
+    }
+
+    private boolean pertence(NoArvoreBinaria<T> no, T info) {
+        if (no == null) {
+            return false;
+        }
+
+        return no.getInfo() == info || pertence(no.getEsquerda(), info) || pertence(no.getDireita(), info);
+    }
+
+    public boolean pertence(T info) {
+        return pertence(raiz, info);
+    }
+
+    private String arvorePre(NoArvoreBinaria<T> no) {
+
+        if (no == null) {
+            return "<>";
+        }
+
+        return "<" + no.getInfo() + arvorePre(no.getEsquerda()) + arvorePre(no.getDireita()) + ">";
+
+    }
+
+    public String toString() {
+        return arvorePre(raiz);
+    }
+
+    private int contarNos(NoArvoreBinaria<T> no) {
+
+        if (no == null) {
+            return 0;
+        }
+
+        return contarNos(no.getEsquerda()) + contarNos(no.getDireita()) + 1;
+    }
+
+    public int contarNosInternos(NoArvoreBinaria<T> sa){
+        if (sa == null) {
+            return 0;
+        }
+
+        if(sa.getEsquerda() == null && sa.getDireita() == null){
+            return 0;
+        }
+
+        return contarNosInternos(sa.getEsquerda()) + contarNosInternos(sa.getDireita()) + 1;
+    }
+
+    public int contarNos() {
+        return contarNos(raiz);
+    }
+    
+    private int contarFolhas(NoArvoreBinaria<T> sa){
+        int folhas = 0;
+        if(sa.getEsquerda() == null && sa.getDireita() == null){
+            return folhas += 1;
+        } else {
+            if(sa.getDireita() != null){
+                folhas += contarFolhas(sa.getDireita());
+            }
+            
+            if(sa.getEsquerda() != null){
+                folhas += contarFolhas(sa.getEsquerda());
+            }
+        }
+        return folhas;
+    }
+
+}
---
+++ EstruturaDeDados/src/UnidadeUm/Arvore/exercicio8/Arvore.java
@@ -0,0 +1,131 @@
+package UnidadeUm.Arvore.exercicio8;
+
+public class Arvore<T> {
+
+    private NoArvore<T> raiz;
+
+    public Arvore() {
+        setRaiz(null);
+    }
+
+    public NoArvore<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NoArvore<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    private boolean pertence(NoArvore<T> no, T info) {
+        if (no.getInfo().equals(info)) {
+            return true;
+        }
+
+        NoArvore<T> p = no.getPrimeiro();
+
+        while (p != null) {
+            if (pertence(p, info)) {
+                return true;
+            }
+            p = p.getProximo();
+        }
+
+        return false;
+    }
+
+    public boolean pertence(T info) {
+        if (getRaiz() == null) {
+            return false;
+        }
+
+        return pertence(getRaiz(), info);
+    };
+
+    private String obterRepresentacaoTextual(NoArvore<T> no) {
+        String s = "<";
+
+        s += no.getInfo();
+
+        NoArvore<T> p = no.getPrimeiro();
+
+        while (p != null) {
+            s += obterRepresentacaoTextual(p);
+            p = p.getProximo();
+        }
+
+        s += ">";
+
+        return s;
+    }
+
+    public String toString() {
+        if (getRaiz() == null) {
+            return "";
+        }
+
+        return obterRepresentacaoTextual(getRaiz());
+    }
+
+    private int ContarNos(NoArvore<T> no) {
+        int i = 1;
+
+        NoArvore<T> p = no.getPrimeiro();
+
+        while (p != null) {
+            i += ContarNos(p);
+            p = p.getProximo();
+        }
+
+        return i;
+    }
+
+    public boolean isDegenerada() {
+        if (getRaiz() == null) {
+            return false;
+        }
+
+        NoArvore<T> p = getRaiz();
+
+        while (p != null) {
+            if (p.getProximo() != null) {
+                return false;
+            }
+
+            p = p.getPrimeiro();
+        }
+
+        return true;
+
+    }
+
+    public int ContarNos() {
+        if (getRaiz() == null) {
+            return 0;
+        }
+
+        return ContarNos(getRaiz());
+    }
+
+    public int contarNosInternos() {
+        if (raiz == null) return 0;
+        return contarNosInternos(raiz);
+    }
+    
+    private int contarNosInternos(NoArvore<T> no) {
+        if (no == null) return 0;
+    
+        int count = 0;
+        if (no.getPrimeiro() != null) {
+            count = 1; 
+        }
+    
+        NoArvore<T> p = no.getPrimeiro();
+        while (p != null) {
+            count += contarNosInternos(p);
+            p = p.getProximo();
+        }
+    
+        return count;
+    }
+
+}
