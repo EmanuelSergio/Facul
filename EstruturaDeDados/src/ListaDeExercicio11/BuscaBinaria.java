ace3cc0fdee586f556ba3029655d3e6024d34825
Emanuel Sergio Girardi <esgirardi@furb.br>
2025-05-28 21:29:07 -0300

    comeco da lista 11

---
+++ EstruturaDeDados/src/ListaDeExercicio11/BuscaBinaria.java
@@ -0,0 +1,17 @@
+package ListaDeExercicio11;
+
+public class BuscaBinaria<T extends Comparable<T>> extends BuscaAbstract {
+    
+    
+    public int Buscar(T valor) {
+        Object[] info = getInfo();
+        int inicio = 0;
+        int fim = info.length - 1;
+        int meio = (inicio + fim) / 2;
+
+        
+
+
+        return -1; 
+    }
+}
